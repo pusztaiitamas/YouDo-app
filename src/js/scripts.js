5a3b7e2cdd6547343a168a6fb445d85ac35df5d3
Pusztai Tamas <pusztaiitamas@gmail.com>
2021-02-06 10:48:09 +0100

    MZ3

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,7 +5,12 @@ const createYouDoText =document.getElementById("youdo_text")
 formElement.addEventListener("submit", function (ev) {
     event.preventDefault();
     const newYouDo = document.createElement("li");
+    const deleteButton = document.createElement("button");
     formElement.append(newYouDo);
     newYouDo.innerText = createYouDoText.value
-
+    newYouDo.append(deleteButton);
+    deleteButton.innerHTML = " <i class=\"fas fa-trash\"></i>";
+    deleteButton.addEventListener("click",function (ev){
+        createYouDoText.parentNode.removeChild(newYouDo);
+    })
 })
