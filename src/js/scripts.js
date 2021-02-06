58fa6253d6c3d642b51ec95b5b4b9f75c26b4394
Pusztai Tamas <pusztaiitamas@gmail.com>
2021-02-06 11:54:45 +0100

    MZ4

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,11 @@
 const formElement = document.getElementById("form_element");
 const ulElement = document.getElementById("youdo_list");
-const createYouDoText =document.getElementById("youdo_text")
+const createYouDoText = document.getElementById("youdo_text");
 
-formElement.addEventListener("submit", function (ev) {
+createYouDoText.setAttribute("required", "");
+createYouDoText.setAttribute("pattern", ".{6,}");
+console.log(createYouDoText);
+formElement.addEventListener("submit", function (event) {
     event.preventDefault();
     const newYouDo = document.createElement("li");
     const deleteButton = document.createElement("button");
@@ -10,7 +13,17 @@ formElement.addEventListener("submit", function (ev) {
     newYouDo.innerText = createYouDoText.value
     newYouDo.append(deleteButton);
     deleteButton.innerHTML = " <i class=\"fas fa-trash\"></i>";
-    deleteButton.addEventListener("click",function (ev){
+    deleteButton.addEventListener("click", function (ev) {
         createYouDoText.parentNode.removeChild(newYouDo);
     })
+    checkValidity(createYouDoText)
 })
+
+function checkValidity(element) {
+    if (element.validity.valid === false){
+        element.style.border = "3px solid red"
+    }else {
+        element.style.border = "3px solid green"
+    }
+        }
+
