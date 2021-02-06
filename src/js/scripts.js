55e558993163fd7516b4936410c03ef862bec40d
Pusztai Tamas <pusztaiitamas@gmail.com>
2021-02-06 10:13:03 +0100

    MZ2

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,11 @@
-//can be deleted
-console.log("Console log works!");
+const formElement = document.getElementById("form_element");
+const ulElement = document.getElementById("youdo_list");
+const createYouDoText =document.getElementById("youdo_text")
+
+formElement.addEventListener("submit", function (ev) {
+    event.preventDefault();
+    const newYouDo = document.createElement("li");
+    formElement.append(newYouDo);
+    newYouDo.innerText = createYouDoText.value
+
+})
